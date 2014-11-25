307a852bae499a243bbf6d92416ee015bf952291
Chad Paulson <chad.paulson@gmail.com>
2014-11-24 21:52:19 -0600

    Adds github ribbon.  Changes default port.

--- react/src/app.jsx
+++ react/src/app.jsx
@@ -43,6 +43,7 @@ var App = React.createClass({
           <link rel="stylesheet" type="text/css" href="/css/style.css" />
         </head>
         <body>
+        <a href="https://github.com/chadpaulson/react-isomorphic-video-game-search"><img className="github-ribbon" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" /></a>
         <Search onSearch={this.searchGames} entryPath={this.state.entryPath} />
         <DocumentTitle title="%react-iso-vgs%">
         <CaptureClicks>
--- react/src/config/index.js
+++ react/src/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  LOCAL_API_HOST: 'http://localhost:3000',
+  LOCAL_API_HOST: 'http://localhost:4000',
   REMOTE_API_HOST: 'www.giantbomb.com',
   GIANT_BOMB_API_KEY: '966f78bbd84fb51dcc37c4d125bddd5148401b5f' // please change
 }
